9e36735a3dde9ff2b5f89d7ea111d51f1bafd51a
Esau Kang <esaugkang@gmail.com>
2014-05-24 11:58:47 -0700

    Hi

---
+++ com/ltkernel/screens/Play.java
@@ -0,0 +1,47 @@
+package com.ltkernel.screens;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
+
+/**
+ * Created by esauKang on 5/24/14.
+ */
+public class Play implements Screen {
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
+
