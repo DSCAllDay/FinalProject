b311fe14a87dd14746f5f34621fcd78da86b9593
monty816 <tommyboymadden@yahoo.com>
2014-05-28 13:40:04 -0500

    PistolBullet v. 0.1.1

--- com/ltkernel/entities/PistolBullet.java
+++ com/ltkernel/entities/PistolBullet.java
@@ -1,13 +1,17 @@
-package com.ltkernel.entities;
+package com.libgdxgaming.angryshooter.Objects;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.math.*;
-import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.physics.bullet.*;
-import com.ltkernel.screens.*;
+import com.badlogic.gdx.math.Vector2;
 
-/**
- * Created by tommy_000 on 5/28/2014.
- */
-public class PistolBullet extends Bullet {
+public class PistolBullet extends Object {
+    public PistolBullet(float x, float y, Vector2 velocity) {
+        super(x, y);
+
+        this.velocity = velocity;
+    }
+
+    // We must override the original method in order to make the bullet go a little faster
+    @Override
+    public void update() {
+        position.add(velocity.x * 10, velocity.y * 10);
+    }
 }
